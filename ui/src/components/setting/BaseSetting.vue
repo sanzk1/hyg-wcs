0de874d159d9a4cbc716719b81db1bdff6597896
Hygge <53632983+Hygge@users.noreply.github.com>
2024-12-06 21:59:23 +0800

    新增系统设置配置页面

---
+++ ui/src/components/setting/BaseSetting.vue
@@ -0,0 +1,74 @@
+<script setup>
+import {ref} from "vue";
+import { uploadOutlined, } from '@ant-design/icons-vue';
+
+const previewFile = async file => {
+  console.log('Your upload file:', file);
+  const res = await fetch('https://next.json-generator.com/api/json/get/4ytyBoLK8', {
+    method: 'POST',
+    body: file,
+  });
+  const { thumbnail } = await res.json();
+  return thumbnail;
+};
+const fileList = ref([]);
+const gap = ref(20)
+
+</script>
+
+<template>
+
+  <a-flex :gap="gap" vertical style="padding: 50px;">
+    <a-flex style="width: 400px;" :gap="gap">
+      <span style="width: 120px;font-weight: bold;">网站名称：</span>
+      <a-input  placeholder="请输入网站名称" />
+    </a-flex>
+    <a-flex style="width: 400px;" :gap="gap">
+      <span style="width: 120px;font-weight: bold;">登录页名称：</span>
+      <a-input  placeholder="请输入登录页名称" />
+    </a-flex>
+    <a-flex style="width: 400px;" :gap="gap">
+      <span style="width: 120px;font-weight: bold;">主页版权：</span>
+      <a-input  placeholder="请输入" />
+    </a-flex>
+    <a-flex style="width: 400px;" :gap="gap">
+      <span style="width: 120px;font-weight: bold;">登录页LOGO：</span>
+      <a-upload
+          v-model:file-list="fileList"
+          list-type="picture"
+          action="//jsonplaceholder.typicode.com/posts/"
+          :preview-file="previewFile"
+      >
+        <a-button>
+          <upload-outlined></upload-outlined>
+          Upload
+        </a-button>
+      </a-upload>
+    </a-flex>
+   <a-flex style="width: 400px;" :gap="gap">
+      <span style="width: 120px;font-weight: bold;">后台LOGO：</span>
+         <a-upload
+             v-model:file-list="fileList"
+             list-type="picture"
+             action="//jsonplaceholder.typicode.com/posts/"
+             :preview-file="previewFile"
+         >
+           <a-button>
+             <upload-outlined></upload-outlined>
+             Upload
+           </a-button>
+         </a-upload>
+    </a-flex>
+
+
+    <a-button type="primary" style="width: 200px;margin-top: 100px;"  >保存</a-button>
+
+  </a-flex>
+
+</template>
+
+<style scoped>
+
+
+
+</style>
--- ui/src/views/admin/setting/index.vue
+++ ui/src/views/admin/setting/index.vue
@@ -1,15 +1,27 @@
 <script setup>
 
+import { ref } from 'vue';
+import BaseSetting from "@/components/setting/BaseSetting.vue";
 
 defineOptions({
   name : 'setting'
 })
 
+const tabPosition = ref('top');
+const activeKey = ref('1');
 
 </script>
 
 <template>
-  <h1>系统设置</h1>
+  <a-card>
+
+    <a-tabs v-model:activeKey="activeKey" :tab-position="tabPosition" animated>
+      <a-tab-pane key="1" tab="基础配置"><BaseSetting /></a-tab-pane>
+      <a-tab-pane key="2" tab="基础配置">Content of Tab 2</a-tab-pane>
+      <a-tab-pane key="3" tab="基础配置">Content of Tab 3</a-tab-pane>
+    </a-tabs>
+
+  </a-card>
 
 </template>
 
